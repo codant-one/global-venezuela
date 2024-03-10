dd2829dd5878f763235957c14d0de12c1d665310
SteffaniCastro <steffani.castro.useche@gmail.com>
2024-03-10 19:25:37 -0400

    fix pages

---
+++ frontend/src/pages/dashboard/admin/inmigrants/add/index.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+</script>
+
+<template>
+    <section>
+        crear
+    </section>
+</template>
+
+<route lang="yaml">
+    meta:
+      action: crear
+      subject: inmigrantes
+  </route>
---
+++ frontend/src/pages/dashboard/admin/inmigrants/edit/[id].vue
@@ -0,0 +1,15 @@
+<script setup>
+
+</script>
+
+<template>
+    <section>
+        editar
+    </section>
+</template>
+
+<route lang="yaml">
+    meta:
+      action: editar
+      subject: inmigrantes
+  </route>
