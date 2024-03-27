448a63bfcbeb0e93b03b8da2853d7d0a411d369f
SteffaniCastro <steffani.castro.useche@gmail.com>
2024-03-27 11:23:26 -0400

    fix independents volunteer

--- backend/app/Imports/MigrantImport.php
+++ backend/app/Imports/MigrantImport.php
@@ -59,31 +59,30 @@ class MigrantImport implements ToModel
         $parish_id = $parishes_arr[$key]['id'];
         $unixDate = ($row[9] - 25569) * 86400; // 25569 es el número de días desde la fecha base de Excel hasta la fecha base de Unix (1 de enero de 1970)
         $date = gmdate('Y-m-d', $unixDate);
-        echo print_r($row[6]);
-        echo print_r($country[0]['id']);
-        // return new Migrant([
-        //     'country_id' => $country[0]['id'],
-        //     'user_id' => 1,
-        //     'gender_id' => $gender_id,
-        //     'parish_id' => $parish_id,
-        //     'community_council_id' => null,
-        //     'name' => mb_convert_case(mb_strtolower($row[6]), MB_CASE_TITLE, "UTF-8"),
-        //     'last_name' => mb_convert_case(mb_strtolower($row[7]), MB_CASE_TITLE, "UTF-8"),
-        //     'email' => $row[8],
-        //     'birthdate' => $date,
-        //     'passport_number' => $row[10],
-        //     'passport_status' => ($row[11] === 'SI') ? 1 : 0,
-        //     'transient' => ($row[12] === 'SI') ? 1 : 0,
-        //     'resident' => ($row[13] === 'SI') ? 1 : 0,
-        //     'process_saime' => ($row[14] === 'SI') ? 1 : 0,
-        //     'years_in_country' => $row[15],
-        //     'antecedents' => ($row[16] === 'SI') ? 1 : 0,
-        //     'isMarried' => ($row[17] === 'SI') ? 1 : 0,
-        //     'has_children' => ($row[18] === 'SI') ? 1 : 0,
-        //     'children_number' => ($row[19] === null || $row[19] === '') ? null : $row[19],
-        //     'phone' => str_replace('-', '', $row['20']) ?? null,
-        //     'address' => $row[21],
-        //     'file_document' => 'migrants/'.$row[22] ?? null
-        // ]);
+
+        return new Migrant([
+            'country_id' => $country[0]['id'],
+            'user_id' => 1,
+            'gender_id' => $gender_id,
+            'parish_id' => $parish_id,
+            'community_council_id' => null,
+            'name' => mb_convert_case(mb_strtolower($row[6]), MB_CASE_TITLE, "UTF-8"),
+            'last_name' => mb_convert_case(mb_strtolower($row[7]), MB_CASE_TITLE, "UTF-8"),
+            'email' => $row[8],
+            'birthdate' => $date,
+            'passport_number' => $row[10],
+            'passport_status' => ($row[11] === 'SI') ? 1 : 0,
+            'transient' => ($row[12] === 'SI') ? 1 : 0,
+            'resident' => ($row[13] === 'SI') ? 1 : 0,
+            'process_saime' => ($row[14] === 'SI') ? 1 : 0,
+            'years_in_country' => $row[15],
+            'antecedents' => ($row[16] === 'SI') ? 1 : 0,
+            'isMarried' => ($row[17] === 'SI') ? 1 : 0,
+            'has_children' => ($row[18] === 'SI') ? 1 : 0,
+            'children_number' => ($row[19] === null || $row[19] === '') ? null : $row[19],
+            'phone' => str_replace('-', '', $row['20']) ?? null,
+            'address' => $row[21],
+            'file_document' => 'migrants/'.$row[22] ?? null
+        ]);
     }
 }
